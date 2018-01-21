6375dd9eced52c0c500f65b32a3eccf02be26abc
Jeka Lion <djoni-01@outlook.com>
2018-01-21 17:50:04 +0600

    init

---
+++ RunningButton/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RunningButton
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private double maxHeight;
+        //private double maxWidth;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //maxHeight = button.ActualHeight;
+            //maxWidth = button.ActualWidth;
+        }
+
+        private void ButtonMouseMove(object sender, MouseEventArgs e)
+        {
+            //Random rand = new Random();
+            if (button.HorizontalAlignment == HorizontalAlignment.Left)
+            {
+                button.HorizontalAlignment = HorizontalAlignment.Right;
+                if (button.VerticalAlignment == VerticalAlignment.Top)
+                {
+                    button.VerticalAlignment = VerticalAlignment.Bottom;
+                }
+                else
+                {
+                    button.VerticalAlignment = VerticalAlignment.Top;
+                }
+            }
+            else
+            {
+                button.HorizontalAlignment = HorizontalAlignment.Left;
+                if (button.VerticalAlignment == VerticalAlignment.Top)
+                {
+                    button.VerticalAlignment = VerticalAlignment.Bottom;
+                }
+                else
+                {
+                    button.VerticalAlignment = VerticalAlignment.Top;
+                }
+            }
+            //button.Height = rand.Next((int)maxHeight);
+            //button.Width = rand.Next((int)maxWidth);
+        }
+    }
+}
